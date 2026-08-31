5c6ee3411d89a61066d3539f7aa7d2e3e5015715
ictservic2026-source <ictservic2026@gmail.com>
2026-08-31 16:34:40 +0700

    Delete ICT directory

--- ICT/js/protected-page.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const tag = document.currentScript;
-  const roles = (tag.dataset.roles || '').split(',').map((role) => role.trim()).filter(Boolean);
-  document.documentElement.classList.add('auth-pending');
-  window.ICTAuth.loadProtectedPage(tag.dataset.pageScript, roles);
-})();
